e15ef8dfd5b14219d4115dd688fad7b357035dc3
Kuba Szóstko <kubaszostko@gmail.com>
2020-06-03 07:44:25 +0200

    successfully fetched data

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('dsasdad');
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const baseEndpoint = 'http://www.recipepuppy.com/api/?q=pizza';
+
+fetch(`${proxy}${baseEndpoint}`)
+.then(resp => resp.json())
+.then(data => console.log(data));
